0c6bc42460db94d3447763c27245fc51c8dbfe6f
Angus <angus.baker2468@gmail.com>
2025-01-13 13:56:25 +0000

    The tasts completed

---
+++ app.js
@@ -0,0 +1,21 @@
+console.log("Testing");
+
+const blogPosts = [
+  "My first blog post",
+  "My second blow post",
+  "My third blog post",
+];
+console.log(blogPosts[0]);
+console.log(blogPosts[1]);
+console.log(blogPosts[2]);
+
+blogPosts[3] = "My fourth blog post";
+
+console.log(blogPosts[3]);
+
+const favortieFoods = ["Pizza", "Pasta", "Potatoes"];
+console.log(favortieFoods);
+const favortieColours = ["Pink", "Purple", "Maroon"];
+console.log(favortieColours);
+const favortieNumbers = ["36", "13", "49"];
+console.log(favortieNumbers);
