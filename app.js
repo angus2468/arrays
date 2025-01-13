2b9172c6725b8c77865526ce67b31f215fc1ec54
Angus <angus.baker2468@gmail.com>
2025-01-13 16:04:05 +0000

    tasks finished not arrays yet

--- app.js
+++ app.js
@@ -19,3 +19,22 @@ const favortieColours = ["Pink", "Purple", "Maroon"];
 console.log(favortieColours);
 const favortieNumbers = ["36", "13", "49"];
 console.log(favortieNumbers);
+
+for (let i = 0; i < 5; i++) {
+  console.log("Iteration", i);
+}
+
+let counter = 1;
+
+while (counter <= 5) {
+  console.log(counter);
+  counter++;
+}
+
+let keeplooping = true;
+let i = 0;
+while (keeplooping == true) {
+  console.log("Iteration", i);
+  keeplooping = Math.random() < 0.1;
+  i++;
+}
